d317270d07a7e054177e5fa27a92ebcdfcd3b47b
Olivia (Yefan) Jiang <yefan.jiang@mail.utoronto.ca>
2021-10-15 17:23:13 -0400

    Pushed some additional changes before merging

---
+++ src/test/java/TicketManagerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TicketManagerTest {
+
+    TicketManager tm;
+
+    @Before
+    public void setUp() { tm = new TicketManager();}
+    
+    
+}
