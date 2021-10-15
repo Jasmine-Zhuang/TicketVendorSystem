8ff66275d195a73e7fb78fda8faf448332b46713
Olivia (Yefan) Jiang <yefan.jiang@mail.utoronto.ca>
2021-10-15 10:51:46 -0400

    Added a TicketManager class

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -1,5 +1,5 @@
 /*
-This file defines a class called TicketManager that stores all sold tickets.
+This is the class that manages all sold tickets.
  */
 
 
@@ -19,16 +19,43 @@ public class TicketManager {
         this.soldTickets = new ArrayList<Ticket>();
     }
 
+    /**
+     * A getter method.
+     * @return A list of sold tickets.
+     */
+
     public ArrayList<Ticket> getSoldTickets() {
         return soldTickets;
     }
 
-    public void addToSoldTickets(Ticket ticket) {
-        soldTickets.add(ticket);
+    /**
+     * If a customer booked a ticket, add it to soldTicket.
+     * @param ticket A Ticket instance.
+     * @return a confirmation message confirming if this ticket has been stored, false otherwise.
+     */
+
+    public String addToSoldTickets(Ticket ticket) {
+        if (!soldTickets.contains(ticket)) {
+            soldTickets.add(ticket);
+            return "You have successfully booked the ticket for" + ticket.getFlightNumber() + "! The" +
+                    "details are \n" + ticket.toString();
+        }
+        return "Sorry! Please ensure all the information is correct, and try again later!";
     }
 
-    public void cancelTicket(Ticket ticket) {
-        soldTickets.remove(ticket);
+    /**
+     * Remove a ticket from soldTicket to represent that this ticket has been canceled.
+     * @param ticket A Ticket instance.
+     * @return true if this ticket has been removed from the list, false otherwise.
+     */
+
+    public String removeFromSoldTickets(Ticket ticket) {
+        if (soldTickets.contains(ticket)) {
+            soldTickets.remove(ticket);
+            return "You have successfully canceled the ticket for" + ticket.getFlightNumber() + "! The details are \n" +
+                    ticket.toString();
+        }
+        return "You have not booked this flight yet, so it cannot be cancelled.";
     }
 
 
