0e18601f9796b6394924ad1d96a93b11b15ec922
Jasmine-Zhuang <75867805+Jasmine-Zhuang@users.noreply.github.com>
2021-10-16 11:11:15 -0400

    Update UI.java

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 public class UI {
     public static void main(String[] args) {
@@ -8,8 +7,7 @@ public class UI {
         String option = scanner.nextLine();
         while(!option.equals("Yes") && !option.equals("No")){
             System.out.println("Sorry, your answer is invalid, please make sure you type either Yes or No");
-            System.out.println("Welcome to U-ticket! \n To start, may I ask do you have an account? \n" +
-                    "Please enter Yes or No.");
+            System.out.println( "Please enter Yes or No.");
             option = scanner.nextLine();
         }
         if(option.equals("Yes")){
@@ -24,13 +22,24 @@ public class UI {
            //create an account
            System.out.println("Now let's register to continue. \n Please enter you username");
             String username = scanner.nextLine();
+            // if this id exists.... can use a while loop for this
+           // System.out.println("This id has been used. Please type another one.");
+
             System.out.println("Please enter you password");
             String password = scanner.nextLine();
             //create an account with this info.
     }
-
-    }
-}
+       //Once login succeeds:
+        System.out.println("Login succeeds! \n Please type 1 to manage your account, 2 to book ticket  3 to exit");
+       int option1 = scanner.nextInt();
+       while(option1 !=1 && option1!=2 && option1!=3){
+           System.out.println("Sorry, your answer is invalid, please make sure you type either 1, 2 or 3");
+           option1 = scanner.nextInt();
+       }
+       if(option1==1){}
+       else if(option1==2){}
+       else{}
+}}
 
 
 
