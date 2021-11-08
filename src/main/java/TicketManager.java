3a4c3baabf925c413a4d86725ccbe4c695e2ebed
Olivia (Yefan) Jiang <yefan.jiang@mail.utoronto.ca>
2021-11-08 11:08:49 -0500

    UI modified some prompts.

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -3,20 +3,19 @@ This is the class that manages all sold tickets.
  */
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class TicketManager {
 
 
-    private ArrayList<Ticket> soldTickets;
+    private final ArrayList<Ticket> soldTickets;
 
     /** Construct a TicketManager, giving them an empty list of soldTickets
-     *
-     * @param soldTickets a List of Tickets that have been sold to customers.
      */
 
     public TicketManager() {
-        this.soldTickets = new ArrayList<Ticket>();
+        this.soldTickets = new ArrayList<>();
     }
 
     /**
@@ -34,11 +33,11 @@ public class TicketManager {
      * @return a confirmation message confirming if this ticket has been stored, false otherwise.
      */
 
-    public String addToSoldTickets(Ticket ticket) {
+    public String bookTickets(Ticket ticket) {
         if (!soldTickets.contains(ticket)) {
             soldTickets.add(ticket);
-            return "You have successfully booked the ticket for" + ticket.getFlightNumber() + "! The" +
-                    "details are \n" + ticket.toString();
+            return "You have successfully booked the ticket for flight " + ticket.getFlightNumber() + "! The" +
+                    " details are: \n" + ticket;
         }
         return "Sorry! Please ensure all the information is correct, and try again later!";
     }
@@ -49,14 +48,22 @@ public class TicketManager {
      * @return true if this ticket has been removed from the list, false otherwise.
      */
 
-    public String removeFromSoldTickets(Ticket ticket) {
+    public String cancelTickets(Ticket ticket) {
         if (soldTickets.contains(ticket)) {
             soldTickets.remove(ticket);
-            return "You have successfully canceled the ticket for" + ticket.getFlightNumber() + "! The details are \n" +
-                    ticket.toString();
+            return "You have successfully canceled the ticket for flight " + ticket.getFlightNumber() +
+                    "! The details are:" + " \n" + ticket;
         }
-        return "You have not booked this flight yet, so it cannot be cancelled.";
+        return "You have not booked this flight yet, so it cannot be canceled.";
     }
+
+    public Ticket generateTicket(String flightNumber, String d_city, String a_city, LocalDateTime d_time,
+                                 LocalDateTime a_time, String b_gate, String seat_num, int price,
+                                 String p_name, String p_user) {
+        return new Ticket(flightNumber, d_city, a_city, d_time, a_time, b_gate, seat_num, price, p_name, p_user);
+
+    }
+
 
 
 
