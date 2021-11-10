bbe8b7be3e581664b94a17690baabd0160ae5821
Jasmine-Zhuang <1421179565z@gmail.com>
2021-11-10 16:22:06 -0500

    Create RewardsItemFactory Test

---
+++ src/test/java/UStore/RewardsItemFactoryTest.java
@@ -0,0 +1,16 @@
+package UStore;
+import org.junit.*;
+import static org.junit.Assert.*;
+public class RewardsItemFactoryTest {
+    RewardsItemFactory factory;
+
+    @Test(timeout = 50)
+    public void TestGetItem() {
+        assertTrue(factory.getItem("Backpack") instanceof Backpack);
+        assertTrue(factory.getItem("BookstoreCoupon") instanceof BookstoreCoupon);
+        assertTrue(factory.getItem("GiftCard") instanceof GiftCard);
+        assertTrue(factory.getItem("Mug") instanceof Mug);
+        assertTrue(factory.getItem("TVoucher") instanceof TVoucher);
+        assertNull(factory.getItem(" "));
+       }
+}
