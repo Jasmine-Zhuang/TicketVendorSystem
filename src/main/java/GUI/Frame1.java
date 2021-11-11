42650546f90c9bd27af3e01e4ade6c3a5b84e969
Jasmine-Zhuang <1421179565z@gmail.com>
2021-11-11 10:52:04 -0500

    Create a new class Frame1Demo

--- src/main/java/GUI/Frame1.java
+++ src/main/java/GUI/Frame1.java
@@ -1,16 +1,39 @@
 package GUI;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Frame1 extends JFrame implements ActionListener {
     JButton button;
     Frame1(){
+    //setup button
         button = new JButton();
-        button.setBounds(200,100,100,50);
+        button.setBounds(100,100,250,250);
+
+        //set image for button
+        ImageIcon icon = new ImageIcon("src/main/java/flight_ticket.png");
+        int NEW_WIDTH = 50;
+        int NEW_HEIGHT = 50;
+        Image img = icon.getImage();
+        Image newimg = img.getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  Image.SCALE_SMOOTH ) ;
+        icon = new ImageIcon( newimg );
+        button.setIcon(icon);
+
+        //set text for button
         button.setText("Click this button");
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Comic Sans", Font.BOLD,25));
+
         /*button.addActionListener(this);*/ //will call the actionPerformed method
         button.addActionListener(e -> System.out.println("Clicked"));
+        button.setFocusable(false);// get rid of the line around the button box
+
+
+
+
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
@@ -20,7 +43,7 @@ public class Frame1 extends JFrame implements ActionListener {
 
     }
 
-    /* Invoked when an action occurs.
+  /* Invoked when an action occurs.
      *
      * @param e the event to be processed
      */
@@ -32,6 +55,4 @@ public class Frame1 extends JFrame implements ActionListener {
 
     }
 }
-
-
 
---
+++ src/main/java/GUI/Frame1Demo.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class Frame1Demo {
+    public static void main(String[] args) {
+        new Frame1();
+    }
+}
