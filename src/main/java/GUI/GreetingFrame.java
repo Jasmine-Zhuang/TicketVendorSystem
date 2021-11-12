2014e2e20c7e668e7354269c3b02b6a2975edda7
Jasmine-Zhuang <1421179565z@gmail.com>
2021-11-11 19:35:07 -0500

    Add an images folder

---
+++ src/main/java/GUI/GreetingFrame.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GreetingFrame {
+    GreetingFrame(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
+
+}
