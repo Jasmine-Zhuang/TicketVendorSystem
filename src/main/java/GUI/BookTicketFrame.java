f412722e5fa5fe4f9ece12e5cbb35a110dea38da
Jasmine-Zhuang <1421179565z@gmail.com>
2021-11-13 19:45:30 -0500

    Changes by liuchuan

--- src/main/java/GUI/BookTicketFrame.java
+++ src/main/java/GUI/BookTicketFrame.java
@@ -11,43 +11,50 @@ import Flight.Flight;
 import Flight.FlightManager;
 import Ticket.TicketManager;
 
-public class BookTicketFrame extends JFrame implements ActionListener{
+public class BookTicketFrame extends JFrame implements ActionListener {
     CustomerManager cm;
     FlightManager fm;
     TicketManager tm;
     String flightNum;
-    Customer customer;
+    String customer_user;
     String classType;
     int ticketPrice;
+    String seatNum;
+    Customer customer;
+    PHManager phm;
 
-    String msg = "<html> Passenger Name: " + customer.getName() +
-            "<br/> Flight Number: " + flightNum +
-            "<br/> From: " + fm.getFlightByNum(flightNum).getOriginCity() +
-            "<br/> To: " + fm.getFlightByNum(flightNum).getDestinationCity() +
-            "<br/> Class: " + classType +
-            "<br/> Ticket Price: $" + ticketPrice +
-            "<br/> Balance after consumption: $" + (customer.getBalance()-ticketPrice) +
-            "<br/> Please confirm your Ticket  information." +
-            "<html>";
+
     JButton buttonBook = new JButton("Confirm");
     JButton buttonBack = new JButton("Back");
     JLabel label = new JLabel("Confirm Information");
-    JLabel label2 = new JLabel(msg);
+    JLabel label2 = new JLabel();
     JPanel panel1 = new JPanel();
     JPanel panel2 = new JPanel();
     JPanel panel3 = new JPanel();
 
-    BookTicketFrame(FlightManager fm,CustomerManager cm, TicketManager tm,
-                    String flightNum, Customer c, String classType, int ticketPrice){
+    BookTicketFrame(FlightManager fm, CustomerManager cm, TicketManager tm,
+                    String flightNum, String c, String classType, int ticketPrice, String seatNum, PHManager phm) {
         this.cm = cm;
         this.fm = fm;
         this.tm = tm;
         this.phm=phm;
         this.flightNum = flightNum;
-        this.customer = c;
+        this.customer_user = c;
         this.ticketPrice = ticketPrice;
         this.classType = classType;
-        label.setFont(new Font("Times", Font.BOLD,40));
+        this.seatNum = seatNum;
+        this.customer = cm.showCustomer(c);
+        String msg = "<html> Passenger Name: " + customer.getName() +
+                "<br/> Flight Number: " + flightNum +
+                "<br/> From: " + fm.getFlightByNum(flightNum).getOriginCity() +
+                "<br/> To: " + fm.getFlightByNum(flightNum).getDestinationCity() +
+                "<br/> Class: " + classType +
+                "<br/> Ticket Price: $" + ticketPrice +
+                "<br/> Balance after consumption: $" + (customer.getBalance() - ticketPrice) +
+                "<br/> Please confirm your Ticket  information." +
+                "<html>";
+        label2.setText(msg);
+        label.setFont(new Font("Times", Font.BOLD, 40));
         label.setForeground(Color.white);
         label2.setFont(new Font("Times", Font.PLAIN, 20));
 
@@ -78,18 +85,20 @@ public class BookTicketFrame extends JFrame implements ActionListener{
         this.add(panel3, BorderLayout.SOUTH);
 
 
-
-
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == buttonBack){
+        if (e.getSource() == buttonBack) {
             this.dispose();
-            DisplayPriceFrame dpf = new DisplayPriceFrame(cm, fm, tm);
+            DisplayPriceFrame dpf = new DisplayPriceFrame(cm, fm, tm, seatNum, customer.getUsername(),this.phm);
+        } else {
+            this.dispose();
+            DisplayTicketFrame dtf = new DisplayTicketFrame(fm, cm, tm, flightNum,
+                    fm.getFlightByNum(flightNum).getOriginCity(), fm.getFlightByNum(flightNum).getDestinationCity(),
+                    fm.getFlightByNum(flightNum).getDepartureTime(), fm.getFlightByNum(flightNum).getArrivalTime(),
+                    fm.getFlightByNum(flightNum).getBoardingGate(), seatNum, customer, classType, ticketPrice,this.phm);
         }
-        this.dispose();
 
     }
 
---
+++ src/main/java/GUI/DisplayTicketFrame.java
@@ -0,0 +1,104 @@
+package GUI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+
+import Customer.PHManager;
+import Customer.Customer;
+import Customer.CustomerManager;
+import Flight.Flight;
+import Flight.FlightManager;
+import Ticket.Ticket;
+import Ticket.TicketManager;
+
+public class DisplayTicketFrame extends JFrame implements ActionListener{
+    CustomerManager cm;
+    FlightManager fm;
+    TicketManager tm;
+    PHManager phm;
+    String flightNum;
+    Customer customer;
+    String classType;
+    int ticketPrice;
+    String d_city;
+    String a_city;
+    LocalDateTime d_time;
+    LocalDateTime a_time;
+    String boardingGate;
+    String seatNum;
+    JButton buttonFinish = new JButton("Finish");
+    JLabel label = new JLabel("Ticket Booked!!");
+    JLabel label2 = new JLabel();
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel panel3 = new JPanel();
+
+    DisplayTicketFrame(FlightManager fm, CustomerManager cm, TicketManager tm,
+                       String flightNum, String d_city, String a_city, LocalDateTime d_time,
+                       LocalDateTime a_time, String b_gate, String seat_num,
+                       Customer c, String classType, int ticketPrice, PHManager phm){
+        this.cm = cm;
+        this.fm = fm;
+        this.tm = tm;
+        this.phm=phm;
+        this.flightNum = flightNum;
+        this.customer = c;
+        this.ticketPrice = ticketPrice;
+        this.classType = classType;
+        this.d_city = d_city;
+        this.a_city = a_city;
+        this.d_time = d_time;
+        this.a_time = a_time;
+        this.boardingGate = b_gate;
+        this.seatNum = seat_num;
+        Flight f = fm.getFlightByNum(flightNum);
+        Ticket t = tm.generateTicket(flightNum, d_city, a_city, f.getDepartureTime(),
+                f.getArrivalTime(), b_gate, seat_num, ticketPrice, c.getName(), c.getUsername(), classType);
+        tm.bookTickets(t);
+        fm.reserveSeat(t.getFlightNumber(), t.getSeat_number());
+
+        String msg = t.toString();
+        label2.setText(msg);
+
+        label.setFont(new Font("Times", Font.BOLD,40));
+        label.setForeground(Color.white);
+        label2.setFont(new Font("Times", Font.PLAIN,20));
+
+        buttonFinish.addActionListener(this);
+
+        panel1.setBounds(250, 250, 250, 200);
+        panel1.setBackground(Color.white);
+        panel1.add(label2);
+
+        panel2.setBackground(new Color(0, 76, 153));
+        panel2.setPreferredSize(new Dimension(100, 100));
+        panel2.add(label);
+
+        panel3.setBackground(Color.white);
+        panel3.setPreferredSize(new Dimension(100, 50));
+        panel3.add(buttonFinish);
+
+        this.setTitle("U-Ticket Booking System");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 300);
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+        this.add(panel1);
+        this.add(panel2, BorderLayout.NORTH);
+        this.add(panel3, BorderLayout.SOUTH);
+
+
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.dispose();
+        MainMenuFrame mainMenu = new MainMenuFrame(this.fm,this.cm,this.tm, customer.getUsername(),this.phm);
+    }
+
+}
