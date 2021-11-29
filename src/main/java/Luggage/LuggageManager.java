2eccb1a0fb909519d947e1f756bdaed86bece37c
Chuan Liu <frankchuan.liu@mail.utoronto.ca>
2021-11-29 01:49:21 -0500

    implement Serializable

--- src/main/java/Luggage/LuggageManager.java
+++ src/main/java/Luggage/LuggageManager.java
@@ -1,9 +1,10 @@
 package Luggage;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-public class LuggageManager {
+public class LuggageManager implements Serializable {
 
     private HashMap<String, Luggage> idToLuggage = new LinkedHashMap<>();
     private static final long serialVersionUID = 7;
