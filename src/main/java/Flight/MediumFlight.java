2a2526bb3749b691670fc64e7152ff4a28e46792
Jasmine-Zhuang <1421179565z@gmail.com>
2021-12-02 10:58:10 -0500

    updates constructor of these 3 flight subclasses by adding static variable seatNumArray for each

--- src/main/java/Flight/MediumFlight.java
+++ src/main/java/Flight/MediumFlight.java
@@ -1,15 +1,18 @@
 package Flight;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MediumFlight extends Flight {
     /**
      * Construct a medium Flight.Flight
      */
+    public static ArrayList<String> mediumSeatNumArray = new ArrayList<>(Arrays.asList("1A","1B","2A","2B","3A","3B","4A","4B",
+            "5A","5B","6A","6B","7A","7B","8A","8B","9A","9B","10A","10B"));
     public MediumFlight(String flightNumber, String originCity, String destinationCity, ArrayList<String> departureTime,
-                       ArrayList<String> arrivalTime, int numSeatAvailable, int distance_traveled, String boardingGate,
-                       ArrayList<String> seatNumberArray){
+                       ArrayList<String> arrivalTime, int numSeatAvailable, int distance_traveled, String boardingGate
+                       ){
         super(flightNumber, originCity, destinationCity, departureTime,
-                arrivalTime, 20, numSeatAvailable, distance_traveled, boardingGate,seatNumberArray);
+                arrivalTime, 20, numSeatAvailable, distance_traveled, boardingGate,mediumSeatNumArray);
     }
 }
