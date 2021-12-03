1a78123979dc23d8c8ac0074f6923e1519c16b7a
Jasmine-Zhuang <1421179565z@gmail.com>
2021-12-02 19:40:58 -0500

    update get seat

--- src/main/java/GUI/PickSeatFrame.java
+++ src/main/java/GUI/PickSeatFrame.java
@@ -14,17 +14,14 @@ import java.util.ArrayList;
 
 public class PickSeatFrame extends JFrame implements ActionListener {
     JButton submitButton = new JButton();
-    JTextField inputSeatNum;
-    JTextField inputSeatClass;
     FlightManager fm;
     TicketManager tm;
     CustomerManager cm;
     String flightNum;
-    String seatNumber;
-    String seatClass;
     String username;
     PHManager phm;
     LuggageManager lm;
+    String seatNumber;
 
 
     PickSeatFrame(FlightManager fm,CustomerManager cm, TicketManager tm, String flightNum, String username,
@@ -37,10 +34,15 @@ public class PickSeatFrame extends JFrame implements ActionListener {
         this.phm=phm;
         this.lm = lm;
 
+
+
+
         JPanel panel = new JPanel();
         submitButton.setText("Submit seat picked");
         submitButton.setBounds(200, 300, 100, 30);
-        submitButton.addActionListener(this);
+       /* submitButton.addActionListener(this);*/
+
+
 
         ArrayList<ArrayList<String>> availableSeatArrayList = fm.printAvailableSeat(flightNum);
         JLabel seatDisplayLabel = new JLabel(availableSeatArrayList.toString());
@@ -48,19 +50,19 @@ public class PickSeatFrame extends JFrame implements ActionListener {
         for(int i = 0; i < availableSeatArrayList.size(); i++) {
             availableSeatArray[i] = String.valueOf(availableSeatArrayList.get(i));
         }
-        JComboBox<String> SeatsComoBox = new JComboBox<>(availableSeatArray);
-        SeatsComoBox.setBounds(50, 50, 100, 20);
+        JComboBox<String> SeatsComboBox = new JComboBox<>(availableSeatArray);
+        SeatsComboBox.setBounds(50, 50, 100, 20);
 
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
         panel.add(Box.createHorizontalGlue());
         panel.add(seatDisplayLabel);
         panel.add(Box.createRigidArea(new Dimension(10,10)));
-        panel.add(SeatsComoBox);
+        panel.add(SeatsComboBox);
         panel.add(Box.createRigidArea(new Dimension(10,10)));
         panel.add(submitButton);
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(inputSeatClass);
-        panel.add(inputSeatClass);
+
+
 
 
         this.add(panel);
@@ -69,25 +71,39 @@ public class PickSeatFrame extends JFrame implements ActionListener {
         this.setLocation(new Point(500, 300));
         this.pack();
         this.setVisible(true);
-        this.seatNumber = inputSeatNum.getText();
-        this.seatClass=inputSeatClass.getText();
-    }
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PickSeatFrame.this.seatNumber = SeatsComboBox.getItemAt(SeatsComboBox.getSelectedIndex()).substring(1,3);
+                PickSeatFrame.this.dispose();
+                DisplayPriceFrame displayPriceFrame =
+                        new DisplayPriceFrame(PickSeatFrame.this.cm,PickSeatFrame.this.fm,
+                                PickSeatFrame.this.tm,PickSeatFrame.this.seatNumber,PickSeatFrame.this.username,
+                                PickSeatFrame.this.phm, PickSeatFrame.this.lm);
+                displayPriceFrame.getInfo(PickSeatFrame.this.flightNum,"First");
+            }
+        });
 
-    /**
-     * Invoked when an action occurs.
-     *
-     * @param e the event to be processed
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==submitButton){
-            this.dispose();
-            DisplayPriceFrame displayPriceFrame = new DisplayPriceFrame(this.cm,this.fm,this.tm,this.seatNumber,
-                    this.username,this.phm, this.lm);
-            //flight num, customer usertype, seat type
-            displayPriceFrame.getInfo(flightNum,this.seatClass);
-        }
+
 
     }
+
+//    /**
+//     * Invoked when an action occurs.
+//     *
+//     * @param e the event to be processed
+//     */
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        if(e.getSource()==submitButton){
+//
+//            this.dispose();
+//            DisplayPriceFrame displayPriceFrame = new DisplayPriceFrame(this.cm,this.fm,this.tm,seatNumber,
+//                    this.username,this.phm, this.lm);
+//            //flight num, customer usertype, seat type
+//            displayPriceFrame.getInfo(this.flightNum,"First");
+//        }
+//
+//    }
 
 }
