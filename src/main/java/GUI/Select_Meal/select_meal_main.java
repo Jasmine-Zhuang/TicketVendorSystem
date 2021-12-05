5efc283ec6e327ac733e3e2f26049658528fef15
ryantheF <ryanj.fang@mail.utoronto.ca>
2021-12-05 13:32:48 -0500

    Changed meal GUI size

--- src/main/java/GUI/Select_Meal/select_meal_main.java
+++ src/main/java/GUI/Select_Meal/select_meal_main.java
@@ -84,7 +84,7 @@ public class select_meal_main extends JFrame implements ActionListener {
 
         this.add(panel);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setPreferredSize(new Dimension(350, 320));
+        this.setPreferredSize(new Dimension(500, 320));
         this.setLocation(new Point(500, 300));
         this.pack();
         this.setVisible(true);
